c57527fd5d4968501a9cf1792f8953b87307b63a
Petr Klimov <kapetingi@gmail.com>
2015-09-12 09:57:22 +0300

    - inital commit 1)create spring-boot application 2)adding maven pom dependency

---
+++ topicexpolrer/src/main/java/ru/megaphone/topicexpolrer/application/TopicExplorerApplication.java
@@ -0,0 +1,14 @@
+package ru.megaphone.topicexpolrer.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TopicExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicExplorerApplication.class,args);
+	}
+
+}
